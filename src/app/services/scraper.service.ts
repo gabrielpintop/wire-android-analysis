d8505d2ef20d41dd051ba32ce056beea27737f3a
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2019-04-22 01:38:49 -0500

    Fix for server requests

--- src/app/services/scraper.service.ts
+++ src/app/services/scraper.service.ts
@@ -1,24 +1,32 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScraperService {
+  private options = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
   constructor(private http: HttpClient) {}
 
   getAppData() {
     return new Promise((resolve, reject) => {
-      this.http.get(environment.basePath + 'analysis/wire/info').subscribe(
-        (data: any) => {
-          resolve(data);
-        },
-        err => {
-          console.log(err);
-          return reject(err.error.error);
-        }
-      );
+      this.http
+        .get(environment.basePath + 'analysis/wire/info', this.options)
+        .subscribe(
+          (data: any) => {
+            resolve(data);
+          },
+          err => {
+            console.log(err);
+            return reject(err.error.error);
+          }
+        );
     });
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  basePath: 'http://localhost:8080/api/'
+  basePath: 'https://wire-android-analysis.herokuapp.com/api/'
 };
